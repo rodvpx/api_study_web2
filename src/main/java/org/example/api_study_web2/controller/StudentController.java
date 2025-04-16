ed55d2953b98a46b4139e46a9451b1c8a382bdb7
Rodrigo Simão <rodrigoip435@gmail.com>
2025-04-16 15:47:09 -0300

    feat: implementando o StudentController
    - Implementado StudentController com endpoints para gerenciamento de estudantes
    - Integração com StudentService utilizando DTOs e VOs
    - Mapeamento de rotas e tratamento básico de requisições

--- src/main/java/org/example/api_study_web2/controller/StudentController.java
+++ src/main/java/org/example/api_study_web2/controller/StudentController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/students")
 public class StudentController {
@@ -19,4 +22,28 @@ public class StudentController {
     public ResponseEntity<StudentOutput> save(@RequestBody StudentInput studentInput) {
         return ResponseEntity.status(HttpStatus.CREATED).body(studentService.createStudent(studentInput));
     }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<StudentOutput> update(@PathVariable UUID id, @RequestBody StudentInput studentInput) {
+        return ResponseEntity.status(HttpStatus.OK).body(studentService.updateStudent(id, studentInput));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StudentOutput> getStudent(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(studentService.findById(id));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<StudentOutput>> getAllStudents() {
+        return ResponseEntity.status(HttpStatus.OK).body(studentService.getAllStudents());
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.noContent().build(); // 204 No Content
+    }
+
+
 }
